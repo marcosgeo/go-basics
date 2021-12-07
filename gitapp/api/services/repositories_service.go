e9592276bf974fa83524159ce71c6c752be59050
amarcosgeo <marcosgeo@yahoo.com>
2021-12-07 08:23:23 -0300

    added concurrency to repository_service

--- gitapp/api/services/repositories_service.go
+++ gitapp/api/services/repositories_service.go
@@ -6,43 +6,108 @@ import (
 	"github.com/marcosgeo/go-basics/gitapp/api/domain/repositories"
 	github_provider "github.com/marcosgeo/go-basics/gitapp/api/providers/github"
 	"github.com/marcosgeo/go-basics/gitapp/api/utils/errors"
-	"strings"
+	"net/http"
+	"sync"
 )
 
 type repoService struct{}
 
-type repoServiceInterface interface{
+type repoServiceInterface interface {
 	CreateRepo(request repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError)
+	CreateRepos(request []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.ApiError)
 }
 
 var (
 	RepositoryService repoServiceInterface
 )
 
-func init () {
+func init() {
 	RepositoryService = &repoService{}
 }
 
-func (s *repoService) CreateRepo(
-	input repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError){
-	input.Name = strings.TrimSpace(input.Name)
-	if input.Name == ""{
-		return nil, errors.NewBadRequestError("Invalid repository name")
+func (s *repoService) CreateRepo(input repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.ApiError) {
+	if err := input.Validate(); err != nil {
+		return nil, err
 	}
 
 	request := github.CreateRepoRequest{
-		Name: input.Name,
+		Name:        input.Name,
 		Description: input.Description,
-		Private: false,
+		Private:     false,
 	}
+
 	response, err := github_provider.CreateRepo(config.GetGithubAccessToken(), request)
 	if err != nil {
 		return nil, errors.NewApiError(err.StatusCode, err.Message)
 	}
+
 	result := repositories.CreateRepoResponse{
-		Id: response.Id,
-		Name: response.Name,
+		Id:    response.Id,
+		Name:  response.Name,
 		Owner: response.Owner.Login,
 	}
 	return &result, nil
+}
+
+func (s *repoService) CreateRepos(requests []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.ApiError) {
+	input := make(chan repositories.CreateRepositoriesResult)
+	output := make(chan repositories.CreateReposResponse)
+	defer close(output)
+
+	var wg sync.WaitGroup
+	go s.handleRepoResults(&wg, input, output)
+
+	// 3 requests to process
+	for _, current := range requests {
+		wg.Add(1)
+		go s.createRepoConcurrent(current, input)
+	}
+
+	wg.Wait()
+	close(input)
+
+	result := <-output
+
+	successCreations := 0
+	for _, current := range result.Results {
+		if current.Response != nil {
+			successCreations ++
+		}
+	}
+	if successCreations == 0 {
+		result.StatusCode = result.Results[0].Error.Status()
+	} else if successCreations == len(requests) {
+		result.StatusCode = http.StatusCreated
+	} else {
+		result.StatusCode = http.StatusPartialContent
+	}
+
+	return result, nil
+}
+
+func (s *repoService) handleRepoResults(wg *sync.WaitGroup, input chan repositories.CreateRepositoriesResult, output chan repositories.CreateReposResponse) {
+	var results repositories.CreateReposResponse
+	for incomingEvent := range input {
+		repoResult := repositories.CreateRepositoriesResult{
+			Response: incomingEvent.Response,
+			Error: incomingEvent.Error,
+		}
+		results.Results = append(results.Results, repoResult)
+		wg.Done()
+	}
+	output <- results
+}
+
+func (s *repoService) createRepoConcurrent(input repositories.CreateRepoRequest, output chan repositories.CreateRepositoriesResult) {
+	if err := input.Validate(); err != nil {
+		output <- repositories.CreateRepositoriesResult{Error: err}
+		return
+	}
+
+	result, err := s.CreateRepo(input)
+	if err != nil {
+		output <- repositories.CreateRepositoriesResult{Error: err}
+		return
+	}
+	output <- repositories.CreateRepositoriesResult{Response: result}
 }
